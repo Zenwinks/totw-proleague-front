8bdef34b1fe3263cc8294276bd0a0299454c3b69
Zenwinks <Lulu180299>
2021-05-31 16:42:17 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-view/>
+</template>
+
+<style lang="scss">
+html, body, #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>TOTW Manager</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home'
+}
+</script>
